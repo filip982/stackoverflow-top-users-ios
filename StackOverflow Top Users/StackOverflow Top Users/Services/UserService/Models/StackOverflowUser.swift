3e2a771123c15bc9a8a5e8e2e3b92a649fbf59ad
Filip Miladinovic <filip982@gmail.com>
2026-06-15 23:04:19 +0200

    fix: Decoding HTML test

--- StackOverflow Top Users/StackOverflow Top Users/Services/UserService/Models/StackOverflowUser.swift
+++ StackOverflow Top Users/StackOverflow Top Users/Services/UserService/Models/StackOverflowUser.swift
@@ -19,4 +19,53 @@ public struct StackOverflowUser: Codable, Equatable, Identifiable, Sendable {
         case reputation
         case profileImageURL = "profile_image"
     }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(Int.self, forKey: .id)
+        let rawName = try container.decode(String.self, forKey: .name)
+        name = rawName.decodingHTMLEntities()
+        reputation = try container.decode(Int.self, forKey: .reputation)
+        if let urlString = try container.decodeIfPresent(String.self, forKey: .profileImageURL) {
+            profileImageURL = URL(string: urlString)
+        } else {
+            profileImageURL = nil
+        }
+    }
 }
+
+private extension String {
+    func decodingHTMLEntities() -> String {
+        var result = self
+        let namedEntities: [(String, String)] = [
+            ("&amp;", "&"), ("&lt;", "<"), ("&gt;", ">"),
+            ("&quot;", "\""), ("&apos;", "'"), ("&nbsp;", "\u{00A0}"),
+        ]
+        for (entity, char) in namedEntities {
+            result = result.replacingOccurrences(of: entity, with: char)
+        }
+        // numeric decimal: &#243;
+        var out = ""
+        var i = result.startIndex
+        while i < result.endIndex {
+            if result[i] == "&" {
+                if let semi = result[i...].firstIndex(of: ";"),
+                   result.distance(from: i, to: semi) <= 8 {
+                    let inner = result[result.index(after: i)..<semi]
+                    if inner.hasPrefix("#") {
+                        let digits = inner.dropFirst()
+                        if let code = UInt32(digits), let scalar = Unicode.Scalar(code) {
+                            out.append(Character(scalar))
+                            i = result.index(after: semi)
+                            continue
+                        }
+                    }
+                }
+            }
+            out.append(result[i])
+            i = result.index(after: i)
+        }
+        return out
+    }
+}
+
